bd3ba5d940aa21ffa0770f1eece01421f54ade2f
Ivy Strickland <IvyStrickland@pursuit.org>
2019-04-16 19:37:55 -0400

    created header component

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { HashRouter, Route } from 'react-router-dom';
+
+
+class App extends Component {
+  render() {
+    return (
+      <HashRouter>
+        <>
+        yerrrrrrrrrrrr
+        </>
+      </HashRouter>
+    );
+  }
+}
+
+export default App;
